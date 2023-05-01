e105ee41ac3f3d767c834dd6b0aef39870610aef
LuigiPedata <63244629+GosuMania@users.noreply.github.com>
2023-05-01 17:05:07 +0200

    Fix

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -3,7 +3,6 @@ namespace App\Http\Middleware;
 use Illuminate\Http\Request;
 use Closure;
 class CORS {
-
     public function handle(Request $request, Closure $next) {
         header('Access-Control-Allow-Origin:  *');
         header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, PATCH, DELETE');
