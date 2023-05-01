cc2a7b818e7ef3cba7f42ede5b5b86b43c6f8a70
LuigiPedata <63244629+GosuMania@users.noreply.github.com>
2023-05-01 23:03:11 +0200

    Fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
     public function signInOrSignUp(Request $request)
     {
         $user = User::where('nome', $request->nome)->where('cognome', $request->cognome)->where('nomeUtente', $request->nomeUtente);
-        if($user) {
+        if($user == null) {
             return response()->json(['data' => new UserResource($user)], 200);
         } else {
             $user = User::updateOrCreate(
